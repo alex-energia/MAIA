4741bace2458643448394aec30dd480190c45629
Alexander Rodriguez <energia.alexander@gmail.com>
2026-03-08 15:59:15 -0500

    activar intefaz maia

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from proyectos import proyectos_bp, init_db
 import os
 
@@ -7,23 +7,27 @@ app = Flask(__name__)
 # =========================
 # INICIALIZAR BASE DE DATOS
 # =========================
+
 init_db()
 
 # =========================
 # REGISTRAR BLUEPRINT
 # =========================
+
 app.register_blueprint(proyectos_bp)
 
 # =========================
 # RUTA PRINCIPAL
 # =========================
+
 @app.route("/")
 def home():
-    return "<h1>MAIA funcionando</h1><a href='/proyectos'>Ir a proyectos</a>"
+    return render_template("index.html")
 
 # =========================
 # EJECUTAR APP
 # =========================
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)

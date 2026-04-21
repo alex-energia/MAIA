4e889ed593cd15dd9cc9963a0b75a92269275cb9
Alexander Rodriguez <energia.alexander@gmail.com>
2026-04-20 19:48:46 -0500

    maia viva

--- scout_engine.py
+++ scout_engine.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-# scout_engine.py - MOTOR DE INTELIGENCIA AVANZADO
+# scout_engine.py
 
 def get_market_scout():
-    """Retorna activos con análisis de viabilidad técnica y financiera"""
     return [
         {
             "id": "SC-COL-001",
             "Nombre": "Parque Eólico La Guajira",
             "Ubicación": "Colombia",
-            "Valor": "USD 45M",
+            "Valor_Est": "USD 45M",
             "Riesgo": "BAJO",
             "Viabilidad": 92,
             "Tipo": "Energía Eólica"
@@ -17,7 +16,7 @@ def get_market_scout():
             "id": "SC-CHI-002",
             "Nombre": "Planta Solar Atacama",
             "Ubicación": "Chile",
-            "Valor": "USD 120M",
+            "Valor_Est": "USD 120M",
             "Riesgo": "MEDIO",
             "Viabilidad": 78,
             "Tipo": "Fotovoltaica"
@@ -26,7 +25,7 @@ def get_market_scout():
             "id": "SC-ARG-003",
             "Nombre": "Hidroeléctrica Río Negro",
             "Ubicación": "Argentina",
-            "Valor": "USD 85M",
+            "Valor_Est": "USD 85M",
             "Riesgo": "ALTO",
             "Viabilidad": 45,
             "Tipo": "Hidráulica"
@@ -35,7 +34,7 @@ def get_market_scout():
             "id": "SC-ISL-004",
             "Nombre": "Complejo Geotérmico",
             "Ubicación": "Islandia",
-            "Valor": "USD 210M",
+            "Valor_Est": "USD 210M",
             "Riesgo": "MÍNIMO",
             "Viabilidad": 98,
             "Tipo": "Geotermia"

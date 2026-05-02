ab9d6dc31cc2951917ffff0562beec12ef495b5e
Alexander Rodriguez <energia.alexander@gmail.com>
2026-05-02 18:35:03 -0500

    maia viva

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-(
-echo import flet as ft
-echo import os
-echo import datos_proyecto
-echo def main(page: ft.Page^):
-echo     page.title = "MAIA"
-echo     page.add(datos_proyecto.get_content(^) ^)
-echo     page.update(^)
-echo if __name__ == "__main__":
-echo     port = int(os.getenv("PORT", 8080^) ^)
-echo     ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port^)
-) > main.py
+import flet as ft
+import os
+import datos_proyecto
+def main(page: ft.Page):
+    page.title = "MAIA"
+    page.add(datos_proyecto.get_content() )
+    page.update()
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080) )
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port)
